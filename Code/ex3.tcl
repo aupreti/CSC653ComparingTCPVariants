0de71b6dfd3ea137bcb7d6efce884484537882fe
aupreti <upreti.angela@gmail.com>
2017-10-25 14:09:34 -0400

    Exp3 edits

--- Code/ex3.tcl
+++ Code/ex3.tcl
@@ -21,11 +21,11 @@ set n4 [$ns node]
 set n5 [$ns node]
 set n6 [$ns node]
 
-$ns duplex-link $n1 $n2 10Mb 0ms $alg
+$ns duplex-link $n1 $n2 10Mb 0ms DropTail
 $ns duplex-link $n2 $n3 10Mb 0ms $alg
-$ns duplex-link $n3 $n4 10Mb 0ms $alg
-$ns duplex-link $n3 $n6 10Mb 0ms $alg
-$ns duplex-link $n5 $n2 10Mb 0ms $alg
+$ns duplex-link $n3 $n4 10Mb 0ms DropTail
+$ns duplex-link $n3 $n6 10Mb 0ms DropTail
+$ns duplex-link $n5 $n2 10Mb 0ms DropTail
 
 if {$variant eq "Tahoe"} {
     set tcp [new Agent/TCP]
@@ -33,37 +33,37 @@ if {$variant eq "Tahoe"} {
     set tcp [new Agent/TCP/$variant]
 
 }
-##$tcp set class_ 2
+$tcp set class_ 3
 $ns attach-agent $n1 $tcp
 set sink [new Agent/TCPSink]
 $ns attach-agent $n4 $sink
 $ns connect $tcp $sink
-##$tcp set fid_ 1
+$tcp set fid_ 3
 
 
 set ftp [new Application/FTP]
 $ftp attach-agent $tcp
 $ftp set type_ FTP
-##$ns connect $tcp $sink
 
+$tcp set class_ 4
 set udp [new Agent/UDP]
 $ns attach-agent $n5 $udp
 set null [new Agent/Null]
 $ns attach-agent $n6 $null
 $ns connect $udp $null
-$udp set fid_ 2
+$udp set fid_ 4
 
 set cbr [new Application/Traffic/CBR]
 $cbr attach-agent $udp
 $cbr set type_ CBR
 $cbr set packet_size_ 4000
-$cbr set rate_ 8mb
+$cbr set rate_ mb
 $cbr set random_ false
 
 $ns at 0.1 "$ftp start"
-$ns at 1.0 "$cbr start"
-$ns at 7.0 "$cbr stop"
-$ns at 7.5 "$ftp stop"
+$ns at 3 "$cbr start"
+$ns at 10.0 "$cbr stop"
+$ns at 10.0 "$ftp stop"
 $ns at 8.0 "finish"
 
 puts "CBR packet size = [$cbr set packet_size_]"
